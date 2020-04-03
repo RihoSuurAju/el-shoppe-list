a419324af6f815ce969e7775aa8d7df3afdae0e3
RihoSuurAju <rihomartin.haavasalu@gmail.com>
2020-04-03 18:01:23 +0300

    incredible

--- server.js
+++ server.js
@@ -1,44 +1,66 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const todayDate = require(__dirname+'/dateController.js');
 const port = 3000;
+
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-const items = [];
-const itemsStudy = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model('Item', itemSchema);
 
 app.get('/', (req, res)=>{
-    let day = todayDate.getCurrentDate();
-    res.render('index', {
-        listTitle: day,
-        newListItems: items
-    })
+    let today = new Date();
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    
+    let day = today.toLocaleDateString("en-US", options);
+
+    Item.find({}, function(error, fetchedItems){
+        console.log(fetchedItems);
+        res.render('index', {
+            listTitle: day,
+            newListItems: fetchedItems
+        });
+    });
 });
 
 app.post('/', (req, res)=>{
-    let item = req.body.newItem;
-    if(req.body.list === "Study TODO"){
-        itemsStudy.push(item);
-        res.redirect('/study');
-    } else {
-        items.push(item)
-        res.redirect('/');
-    }
-    console.log(items);
+    console.log(req.body.list);
+    let userInput = req.body.newItem;
+
+    const item = new Item({
+        name: userInput
+    });
+
+    item.save();
+    res.redirect('/');
+   
 });
 
-app.get('/study', (req, res)=>{
-    res.render('index', {
-        listTitle: "Study TODO",
-        newListItems: itemsStudy
-    })
+app.post('/delete', (req, res) => {
+    console.log(req.body.checkbox);
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(error) {
+        if(!error){
+            console.log("Itemvibes: checked.");
+            res.redirect('/');
+        }
+    });
 });
 
-
-app.listen(port, ()=>{
-    console.log("Server is vibing on port " + port);
-});
+app.listen(3000, ()=>{
+    console.log("Server is vibing on port " + port);    
+})
